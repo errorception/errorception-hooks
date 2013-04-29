385e0adfbf19046b58d6706f79a7a59cbd9b79f9
Rakesh Pai <rakeshpai@gmail.com>
2013-04-29 18:18:33 +0530

    Added HipChat integration

---
+++ hooks/hipchat/README.md
@@ -0,0 +1,6 @@
+### HipChat
+
+Sends a message to your HipChat room whever an error occurs.
+
+* Get your API Token from HipChat's [API Tokens](https://www.hipchat.com/admin/api) page. Errorception only needs a notification token, not an admin token.
+* If 'trigger notifications in the room' is checked, HipChat will change the tab color, play a sound, etc. Each recipient's notification preferences are taken into account.
---
+++ hooks/hipchat/index.js
@@ -0,0 +1,48 @@
+var request = require("request");
+
+exports.serviceName = "HipChat";
+
+exports.author = {
+	name: "Rakesh Pai",
+	email: "rakeshpai@errorception.com",
+	github: "errorception",
+	twitter: "errorception"
+};
+
+function formatMessage(error) {
+	var message = "An error " + (error.isFirstOccurrence?"occurred":"recurred") + "!<br />";
+	message += "<b>" + error.message + "</b> ";
+	if(error.isInline) {
+		message += "in an inline script on " + error.page;
+	} else {
+		message += "in " + error.scriptPath;
+	}
+
+	message += " <a href='" + error.webUrl + "'>View details &raquo;</a>";
+
+	return message;
+}
+
+exports.onError = function(error, settings, done) {
+	// Make an API call as documented at https://www.hipchat.com/docs/api/method/rooms/message
+
+	request({
+		url: "https://api.hipchat.com/v1/rooms/message",
+		qs: {
+			auth_token: settings.token
+		},
+		method: "post",
+		form: {
+			room_id: settings.room,
+			from: "Errorception",
+			message_format: "html",
+			message: formatMessage(error),
+			notify: settings.notify,
+			color: settings.color
+		},
+		headers: {
+			"User-Agent": "Errorception Notifications"
+		},
+		timeout: 10000
+	}, done);
+};
