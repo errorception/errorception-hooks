bd5eca05e48e24962a187a51ad7a8fb2f5b95968
Tal Bereznitskey <berzniz@gmail.com>
2013-05-02 10:33:37 +0300

    Added Loggly webhook

---
+++ hooks/loggly/index.js
@@ -0,0 +1,29 @@
+var request = require("request");
+
+exports.serviceName = "Loggly";
+
+exports.author = {
+	name: "Tal Bereznitskey",
+	email: "berzniz@gmail.com",
+	github: "berzniz",
+	twitter: "ketacode"
+};
+
+exports.onError = function(error, settings, done) {
+	// API call docs: http://loggly.com/support/advanced/api-event-submission/
+
+	request({
+		url: "https://logs.loggly.com/inputs/" + settings.serviceKey,
+		headers: {
+			"Content-Type": "application/json",
+			"User-Agent": "Errorception Notifications"
+		},
+		method: "post",
+		body: JSON.stringify({
+			description: "\"" + error.message + "\" caught by Errorception. See " + error.webUrl + " for details",
+			details: error
+		}),
+
+		timeout: 10000
+	}, done);
+};
