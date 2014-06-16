d18470150d78a3f822dc76990283de7064a0e3c4
Rakesh Pai <rakeshpai@gmail.com>
2014-06-16 11:26:14 +0530

    Style tweaks to the Assembla code

--- hooks/assembla/README.md
+++ hooks/assembla/README.md
@@ -1,6 +1,6 @@
-## Assembla
+### Assembla
 
-Creates a ticket in assembla whenever an error occurs.
+Creates a ticket in Assembla whenever an error occurs.
 
-* __Key__ and __Secret__ are found at http://www.assembla.com/user/edit/manage_clients)
-* __Workspace__ is the name of your assembla space/project. Can be found in the url (https://www.assembla.com/spaces/ __workspace__) 
+* Your __Key__ and __Secret__ can be found [here](http://www.assembla.com/user/edit/manage_clients).
+* __Workspace__ is the name of your Assembla space/project. You can find it in the URL. Eg. assembla.com/spaces/__workspace__
--- hooks/assembla/index.js
+++ hooks/assembla/index.js
@@ -1,34 +1,33 @@
 var request = require("request");
 
-exports.serviceName = "assembla";
+exports.serviceName = "Assembla";
 
 exports.author = {
-    name: "Marius Matei",
-    email: "nongeekboy@gmail.com",
-    github: "sica07",
-    twitter: "nongeekboy"
+	name: "Marius Matei",
+	email: "nongeekboy@gmail.com",
+	github: "sica07",
+	twitter: "nongeekboy"
 };
 
 exports.onError = function(error, settings, done) {
-    request({
-        //url: "https://" + settings.client_id + ":" + settings.client_secret + "api.assembla.com/v1/spaces/" + settings.space_id +  "/tickets.json",
-        url: "https://api.assembla.com/v1/spaces/" + settings.workspace +  "/tickets.json",
-        headers: {
-            "X-Api-Key": settings.key,
-            "X-Api-Secret": settings.secret,
-            "Content-Type": "application/json",
-            "User-Agent": "Errorception Notifications"
-        },
-        method: "post",
-        body: JSON.stringify({
-            "ticket": {
-            "summary": "JS Error:" + error.message,
-            "description": "Errorception found an error! \"" + error.message + "\" in "
-            + (error.isInline?("an inline script on " + error.page):error.scriptPath)
-            + ". Details here: " + error.webUrl,
-            "pritority": 2
-        }}),
-        timeout: 10000
-    }, done);
+	request({
+		url: "https://api.assembla.com/v1/spaces/" + settings.workspace +  "/tickets.json",
+		headers: {
+			"X-Api-Key": settings.key,
+			"X-Api-Secret": settings.secret,
+			"Content-Type": "application/json",
+			"User-Agent": "Errorception Notifications"
+		},
+		method: "post",
+		body: JSON.stringify({
+			ticket: {
+				summary: "JS Error:" + error.message,
+				description: "Errorception found an error! \"" + error.message + "\" in "
+					+ (error.isInline?("an inline script on " + error.page):error.scriptPath)
+					+ ". Details here: " + error.webUrl,
+				pritority: settings.priority
+			}
+		}),
+		timeout: 10000
+	}, done);
 }
-
