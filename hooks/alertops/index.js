71233fbe7f46111e475d1980f3f4bda5bf8eb3a9
alertops <xinlinr@alertops.com>
2015-05-06 09:54:12 -0500

    Update index.js
    update shortText and LongText format

--- hooks/alertops/index.js
+++ hooks/alertops/index.js
@@ -13,17 +13,19 @@ exports.author = {
 exports.onError = function(error, settings, done) {
     request({
 		url: "https://notify.alertops.com/restapi.svc/POSTAlertV2/generic/" 
-			 settings.apiKey  "/Errorception/"  settings.sourceName 
-			 "/subject/id/none/none/webUrl/shortText/LongText",
+			+ settings.apiKey + "/Errorception/" + settings.sourceName 
+			+ "/subject/id/none/none/webUrl/shortText/LongText",
 		method: "post",
 		body: JSON.stringify({
-			subject: "(Errorception) "  error.message,
+			subject: "(Errorception) " + error.message,
 			id: path.basename(error.webUrl),
 			webUrl: error.webUrl,
-			shortText: "Error: "  error.message  " at time: "  error.date,
-			LongText: "An Error "  error.message  (error.isFirstOccurrence ? "occurred" : "recurred")  " in "
-						 (error.isInline?("an inline script on "  error.page):error.scriptPath)
-						 " See "  error.webUrl  " for details"
+			shortText: "Error: " + error.message,
+			LongText: "An Error " + error.message + (error.isFirstOccurrence ? " occurred" : " recurred") 
+						+ " at time: " + error.date
+						+ " in " + (error.isInline?("an inline script on " + error.page):error.scriptPath)
+						+ " See " + error.webUrl + " for details"
 		}),
 		timeout: 10000
 	}, done);
+}
