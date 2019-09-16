549b1a8592b74300ab174fb9c52e23bc3cc273cf
netjaan <anders.jacobsson@papyrus.com>
2019-09-16 14:30:26 +0200

    Added hook for MS Teams

---
+++ hooks/teams/README.md
@@ -0,0 +1,10 @@
+### Microsoft Teams
+
+Sends a message to your Teams channel whenever an error occurs.
+
+* In the dropdown next to the desired channel's tab, select "Connectors".
+* Search for "Incoming Webhook".
+* Click Configure.
+* Enter a name and click Create.
+* The Incoming Webhook URL is now shown.
+* Click Done.
---
+++ hooks/teams/index.js
@@ -0,0 +1,42 @@
+var request = require("request");
+
+exports.serviceName = "Teams";
+
+exports.author = {
+	name: "Anders Jacobsson",
+	email: "anders.jacobsson@gmail.com",
+	github: "bloomper"
+};
+
+exports.onError = function(error, settings, done) {
+	request({
+		url: settings.url,
+		method: "post",
+		json: {
+			"@type": "MessageCard",
+			"@extensions": "https://schema.org/extensions",
+			"summary": "Errorception",
+			"themeColor": "",
+			"title": "An error " + (error.isFirstOccurrence ? "occurred" : "recurred") + ".",
+			"sections": [
+				{
+					"title": error.message,
+					"text": (error.isInline ? (" in an inline script on " + error.page) : ("in " + error.scriptPath)),
+					"potentialAction": [
+						{
+							"@type": "OpenUri",
+							"name": "View details",
+							"targets": [
+								{
+									"os": "default",
+									"uri": error.webUrl
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		timeout: 10000
+	}, done);
+};
